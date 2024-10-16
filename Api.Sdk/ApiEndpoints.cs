e6e774f0b63c2a9752c673de9051a54a7625bca5
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-16 12:56:27 +0300

    Add initial API SDK with endpoints and interfaces
    Introduced the `ApiEndpoints` class to manage API endpoints for movies, ratings, and genres. Created the `IMoviesApi` interface with methods for interacting with these endpoints. Added a project file and a `.gitignore` for the SDK.

---
+++ Api.Sdk/ApiEndpoints.cs
@@ -0,0 +1,37 @@
+﻿namespace MoviesHub.Api.Sdk;
+
+public static class ApiEndpoints
+{
+    private const string ApiBase = "/api";
+    
+    public static class Movies
+    {
+        private const string Base = $"{ApiBase}/movies";
+
+        public const string Create = Base;
+        public const string Get = $"{Base}/{{idOrSlug}}";
+        public const string GetAll = Base;
+        public const string Update = $"{Base}/{{id}}";
+        public const string Delete = $"{Base}/{{id}}";
+
+        public const string Rate = $"{Base}/{{id}}/ratings";
+        public const string DeleteRating = $"{Base}/{{id}}/ratings";
+    }
+    
+    public static class Ratings
+    {
+        private const string Base = $"{ApiBase}/ratings";
+
+        public const string GetUserRatings = $"{Base}/me";
+    }
+    
+    public static class Genres
+    {
+        private const string Base = $"{ApiBase}/genres";
+        public const string Create = Base;
+        public const string Get = $"{Base}/{{idOrName}}";
+        public const string GetAll = Base;
+        public const string Update = $"{Base}/{{id}}";
+        public const string Delete = $"{Base}/{{id}}";
+    }
+}
---
+++ Api.Sdk/IMoviesApi.cs
@@ -0,0 +1,50 @@
+﻿using MovieHub.Contracts.Requests;
+using MovieHub.Contracts.Responses;
+using Refit;
+
+namespace MoviesHub.Api.Sdk;
+
+[Headers("Authorization: Bearer")]
+public interface IMoviesApi
+{
+    [Get(ApiEndpoints.Movies.Get)]
+    Task<MovieResponse> GetMovieAsync(string idOrSlug);
+
+    [Get(ApiEndpoints.Movies.GetAll)]
+    Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request);
+    
+    [Post(ApiEndpoints.Movies.Create)]
+    Task<MovieResponse> CreateMovieAsync(CreateMovieRequest request);
+    
+    [Put(ApiEndpoints.Movies.Update)]
+    Task<MovieResponse> UpdateMovieAsync(Guid id, UpdateMovieRequest request);
+    
+    [Delete(ApiEndpoints.Movies.Delete)]
+    Task DeleteMovieAsync(Guid id);
+
+    [Put(ApiEndpoints.Movies.Rate)]
+    Task RateMovieAsync(Guid id, RateMovieRequest request);
+    
+    [Delete(ApiEndpoints.Movies.DeleteRating)]
+    Task DeleteRatingAsync(Guid id);
+
+    [Get(ApiEndpoints.Ratings.GetUserRatings)]
+    Task<IEnumerable<MovieRatingResponse>> GetUserRatingsAsync();
+    
+    [Post(ApiEndpoints.Genres.Create)]
+    Task<GenreResponse> CreateGenreAsync(CreateGenreRequest request);
+    
+    [Get(ApiEndpoints.Genres.Get)]
+    Task<GenreResponse> GetGenreAsync(string idOrName);
+    
+    [Get(ApiEndpoints.Genres.GetAll)]
+    Task<IEnumerable<GenreResponse>> GetAllGenresAsync();
+    
+    [Put(ApiEndpoints.Genres.Update)]
+    Task<GenreResponse> UpdateGenreAsync(string id, UpdateGenreRequest request);
+    
+    [Delete(ApiEndpoints.Genres.Delete)]
+    Task DeleteGenreAsync(string id);
+}
+
+
