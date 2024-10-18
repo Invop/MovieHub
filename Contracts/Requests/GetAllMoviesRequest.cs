6277b7d83f149d8cc9b3753cd0670540b0a6d8d4
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-18 21:30:38 +0300

    Add rating filters to GetAllMoviesRequest
    Added optional MinRating and MaxRating properties to the GetAllMoviesRequest class. This allows users to filter movies based on rating when retrieving all movies.

--- Contracts/Requests/GetAllMoviesRequest.cs
+++ Contracts/Requests/GetAllMoviesRequest.cs
@@ -6,6 +6,8 @@ public class GetAllMoviesRequest : PagedRequest
 
     public int? Year { get; init; }
 
+    public int? MinRating { get; init; }
+    public int? MaxRating { get; init; }
     public IEnumerable<int>? GenreIds { get; init; }
 
     public string? SortBy { get; init; }
